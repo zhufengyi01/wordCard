54e71278107c8b43de20a4f3a16605449f79963b
朱封毅 <673229963@qq.com>
2015-07-02 14:55:36 +0800

    1.第一次创建新的项目

---
+++ Card_iOS/Card_iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Card_iOS
+//
+//  Created by 朱封毅 on 02/07/15.
+//  Copyright (c) 2015年 card. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Card_iOS/Card_iOS/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Card_iOS
+//
+//  Created by 朱封毅 on 02/07/15.
+//  Copyright (c) 2015年 card. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
